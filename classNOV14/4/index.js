3a023d38ca8df3aa3d5a09df35f6f9d2cdb02a85
ali ghodratipour <alireza_ghodratipour@yahoo.com>
2019-11-14 18:35:53 -0700

    NOV14

---
+++ classNOV14/4/index.js
@@ -0,0 +1,18 @@
+
+
+
+ for(var i = 0 ; i < 100 ; i++)
+ if(i%2===0){
+console.log('even')
+ } else  {
+     console.log('odd')
+ }
+//  for(var i=0 ;i<100 ;i++)
+ // if(number[i]%2!==1) 
+ //n{console.log(odd)}
+
+
+ //for(var i=0 ; i<100 ; i++)
+// {console.log[i]}
+
+
