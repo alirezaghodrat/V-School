5920e4aa5530c224fc826429e259d5dbbf7eba63
ali ghodratipour <alireza_ghodratipour@yahoo.com>
2019-11-15 18:48:02 -0700

    classNOV15

---
+++ classNOV15/1/index.js
@@ -0,0 +1,188 @@
+// // for (i=0 ; i<100 ; i++ )
+// //  if(i%2===0){
+// //      console.log(even)
+// //  }else{
+// //      console.log(odd)
+// // //  }
+// // for(i=0;i>100;i++)
+// // if(i%2===0)console.log{
+// // //     (even)
+// // // }
+// //   for(i=0;i<100;i++)
+// //   if(i%2===0){
+// //       console.log('even')}
+// //       else{
+// //           console.log('odd')
+// //       }
+// // for( i = 0 ;i >10 ;i ++ )
+// // if(i%2===0){
+// //     console.log('even')
+// // }else{
+// //     console.log('odd')
+// // }
+
+// // // for( i=0 ;i<10 ;i++ ) {
+// // // console.log(i)}
+// //   //
+// // // for (i=10 ;i>-1; i++ ){
+// // //     console.log(i
+// // // }
+// // for(i=0;i<10;i++){
+// //     console.log(i)
+// // }
+// // //
+// // for(i=9;i>-1;i--){
+// //     console.log(i)
+// // }
+// // //
+// // //
+// // var fruit = ["bnana","ornge","apple","kiwi"]
+
+    
+// // for( var i=0;i<fruit.length;i++){
+// //     console.log(fruit[i])
+// // }
+
+// // //
+// // // var kir =[]
+// // // for( var i=0;i<10;i++){
+// // //     kir.push(i)
+// // // }
+// // // console.log(kir)
+
+// // var kir=[]
+// // for(i=0;i<100;i++){
+// //     kir.push(i)
+// // }
+// // console.log(kir)
+   
+
+// // //
+// // // for( i=0; i<100; i++)
+// // // if(i%2===0)
+// // // {
+// // //     console.log(i)
+// // // }
+
+// // var kir =["bnana","orang","apple","kiwi","peach","pear"]
+// // var result =[]
+// // for(i=0;i<kir.length;i += 2 ){
+// //     result.push(kir[i])
+// // }
+// // console.log(kir)
+// // //
+
+// // for( var i=0;i<101;i++)
+// //     if(i%2===0){
+// // console.log(i)
+// //     }
+// // //
+// // var peopleArray = [
+// //     {
+// //       name: "Harrison Ford",
+// //       occupation: "Actor"
+// //     },
+// //     {
+// //       name: "Justin Bieber",
+// //       occupation: "Singer"
+// //     },
+// //     {
+// //       name: "Vladimir Putin",
+// //       occupation: "Politician"
+// //     },
+// //     {
+// //       name: "Oprah",
+// //       occupation: "Entertainer"
+// //     }
+// //   ]
+  
+  
+// // //   for(var i=0;i<peopleArray.length;i++){
+// // //     console.log(peopleArray[i].occupation)
+
+// // //   }
+
+// // for( i=0; i<peopleArray.length ;i++){
+// //     console.log(peopleArray[i].name)
+// // }
+
+// // //
+// var occupations = []
+// var name = []
+// for(i=0;i<peopleArray.length ;i++){
+//  name.push(peopleArray[i].name)
+// occupations.push(peopleArray[i].occupation)
+// }
+// console.log(name)
+// console.log(occupations)
+
+// // //
+
+// var occupationss = []
+// var names = []
+// for(i=0;i<peopleArray.length ;i++){
+//  names.push(peopleArray[0].name)
+// occupationss.push(peopleArray[1].occupation)
+// }
+// console.log(names)
+// console.log(occupationss)
+
+
+// //
+
+// //function//
+
+// // function sum(numOne,numTow){
+// //     console.log(numOne+numTow)
+// // }
+// // sum(5,10)
+// var sum = function (num1,num2){
+// return num1+num2
+// }
+
+
+// console.log (sum(5,4))
+
+
+// function finder (a,b,c) {
+//     if(a>b&&a>c){
+//         return a
+//     }else if(b>a&&b>c){
+//         return b
+//     }else {
+//         return c
+// }
+// }
+// console.log(finder (9,5,6))
+
+
+
+function ali(a){
+    if ( a % 2 === 0 ){
+        return "even"
+    }else if ( a % 2 !== 0 ){
+        return "odd"
+    }
+
+}
+
+console.log(ali(2))
+
+
+//
+
+var string =[1,2]
+
+function hamid (string){
+     if(string.length[i]<=20){
+         return"string+string"
+     }else (string.length[i]>20) 
+          return"string/2"
+     }
+
+    
+
+    console.log(hamid)
+
+
+ 
