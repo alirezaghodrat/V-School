f7db65702f1c81a9017e0c2a0f01e20d87b2357c
ali ghodratipour <alireza_ghodratipour@yahoo.com>
2019-11-16 22:24:44 -0700

    prN16

---
+++ prN16/index.js
@@ -0,0 +1,307 @@
+// sexvar colors = ["red" , "black" , "green"]
+// // console.log (colors)
+// console.log (colors[1][2])
+
+// //
+
+// var name = [["ali", "hamid" , "gholam"],
+// [1,2,3,4],
+// [true,false]
+// ]
+// console.log(name[0][2])
+
+
+
+// var ali ={
+// sex:"lover",
+// love:"shakoura",
+// son:"shayan",
+// school:"vschool",
+// age:36
+// }
+
+// console.log(ali.age);
+// var person = {
+//     firstName: "Sarah",
+//     lastName: "Smith",
+//     age: 26,
+//     hairColor: "Red",
+//     eyeColor: "Green"
+// }
+ 
+// console.log(person.firstName);
+
+
+// var ali ={
+//     sex: "lover" ,
+//     age:25,
+//     height:185
+// }
+// console.log(ali)
+
+
+
+// var farhad ={
+//     age :35,
+//     man:"no",
+// sex:"gay"
+// }
+
+// console.log(farhad)
+
+
+// var shakoura ={
+//     age:40,
+//     looks:"great",
+//     good:"yes"
+// }
+// console.log(shakoura.age)
+
+
+
+
+// // var age = parseInt(prompt("What is your age?"));
+// // var name = prompt("What is your name?").toLowerCase();
+// // // if (age <= 12 && name === "felix") {
+// // //     // Welcome!
+// // // } else {
+// // //     // Go away!
+// // // var i = 0 ; i < 4; i++) {
+// // //     console.log(favoriteThings[i])
+// // // }
+// // // // for (
+// // // // var favoriteThings = ["Raindrops on roses", "Whiskers on kittens", "Bright copper kettles", "Warm woollen mittens"]
+
+
+
+// // // // var likesRaisins = prompt("Do you like raisins? Type 'yes' or 'no':")
+
+// // // // while(likesRaisins !== "yes" || likesRaisins !== "no"){
+// // // //     likesRaisins = prompt("Please only type 'yes' or 'no'. Do you like raisins?");
+// // // // }
+
+// // // // console.log("You said '" + likesRaisins + "' to my question.")
+
+// // // var eventsAtWork = ["work", "pretend to work", "party", "work", "meeting", "party", "daily grind", "work", "party"]
+// // // for( i = 0; i > eventsAtWork.length ; i++ ){
+// // // console.loge("hooray"[i])
+// // // }
+
+
+// // // var booleans = [true, true, false, true, false, false, false]
+// // // for(i=0;i<booleans.length;i++){
+    
+// // // console.log(booleans[0],[1],[3]);
+
+// // // }
+
+// // // var users = [
+// // //     {
+// // //       name: "Sophie",
+// // //       age: 12
+// // //     },
+// // //     {
+// // // //       name: "Larry",
+// // // //       age: 32
+// // // //     },
+// // // //     {
+// // // //       name: "Cathy",
+// // // //       age: 40
+// // // //     }
+// // // // ]
+// // // // console.log([users])
+// // // // (i=0,i>users.length,i++){
+// // // //     console.log([i].name)
+// // // // }
+ 
+
+// // // // var favoriteThings = ["Raindrops on roses", "Whiskers on kittens", "Bright copper kettles", "Warm woollen mittens"]
+
+// // // // for (var i = 0; i < 4; i++) {
+// // // //     console.log(favoriteThings[i]);
+// // // // }
+
+
+// // // var users = [
+// // //     {
+// // //       name: "Sophie",
+// // //       age: 12
+// // //     },
+// // //     {
+// // //       name: "Larry",
+// // //       age: 32
+// // //     },
+// // //     {
+// // //       name: "Cathy",
+// // //       age: 40
+// // //     }
+// // //   ]
+
+// // // if(i=0,i>user.length,i++)
+// // //   console.log(user)
+
+// // var person = {
+// //     fullName: 'Tommy Boy',
+// //     age: 26,
+// //     weight: 306,
+// //     friends: ['Richard', 'Michelle']
+// // }
+
+// // console.log(person.age)
+
+
+// // var number = a
+// // if (a>0){
+// //   console.log(itsP)
+// // }else {
+// //   console.log(itsN)
+// // }
+
+// for(i=0;i<10;i++) 
+//     if(i%2===0){
+//         console.log("itsEven")
+//     }else{
+//         console.log("itsOdd")
+//     }
+
+// // function sum(a,b){
+// //      return a+b
+// // }
+// // console.log(sum(3,8))
+
+
+
+// // function sum (a,b){
+// //     return a+b
+// // }
+// // console.log(sum(3,4))
+
+// function sum (a,b){
+//     return a+b
+// }
+// console.log(sum(2,3))
+
+
+// function tree (a,b,c){
+//     if( a > b && a > c ){
+//         return (a)
+//     }else if( b > a && b > c ){
+//         return (b) 
+//     }else
+//     return (c)
+// }
+// console.log(tree(3,5,8))
+
+function finder (a,b,c) {
+        if(a>b&&a>c){
+            return a
+        }else if(b>a&&b>c){
+            return b
+        }else {
+            return c
+    }
+    }
+    console.log(finder (9,5,6))
+
+ function ali ( a , b , c ){
+     if( a > b && a > c ){
+         return a
+     }else if ( b > c && b > a ){
+         return b
+      } else {
+             return c
+         }
+     }
+ console.log( ali (3,4,5))
+
+
+
+
+
+
+
+
+
+
+
+
+ function hamid (a,b,c){
+     if( a>b && a>c ){
+         return a
+     }else if( b>a && b>c){
+         return b
+     }else{
+           return c
+     }
+    }
+
+ console.log(hamid(4,6,34))
+
+
+
+
+
+
+ function shayan (a,b,c){
+     if( a > b && a > c ){
+        return a
+     } else if ( b> a && b > c ){
+         return b
+     } else 
+         return c
+     }
+ 
+     console.log (shayan(3,4,555))
+
+
+     //  Write a function that accepts one number as a parameter, and returns whether that number is even or odd. (Return the string "even" or "odd");
+
+     function ali (a){
+            return a%2===0
+     }
+     
+     console.log ( ali(5))
+
+
+     function shakoura (a){
+      if(a%2===0){
+         return "even"
+      }else
+      return "odd"
+     }
+     console.log (shakoura(4))
+
+     function shayan (a,b,c){
+        if( a > b && a > c ){
+           return a
+        } else if ( b> a && b > c ){
+            return b
+        } else 
+            return c
+        }
+    
+        console.log (shayan(3,4,555))
+
+
+        //
+
+        var txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        var sln = txt.length
+
+        function hamid (sln){
+            if(sln<20){
+            return sln+sln
+            }else
+            return sln/2
+        }
+
+        console.log(hamid)
+
+
+
+
+
+
+
+
+
