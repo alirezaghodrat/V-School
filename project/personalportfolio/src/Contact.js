6306c9cbb54abfe8148359018f45c8ba01a096e9
ali ghodratipour <alireza_ghodratipour@yahoo.com>
2020-02-12 21:20:25 -0700

    here is some project

--- project/personalportfolio/src/Contact.js
+++ project/personalportfolio/src/Contact.js
@@ -10,7 +10,7 @@ function Contact(){
                     <img className="imgfooter"src="https://1000logos.net/wp-content/uploads/2017/03/LinkedIn-Logo.png" /></a>
                 <a herf="https://docs.google.com/document/d/1HIHPSGitAsoJ4TZirI2q4JunLAOiAJG1CRm3xzfri0Q/edit?copiedFromTrash#">
                     <img className="imgfooter"src="http://www.pngall.com/wp-content/uploads/2016/04/Resume-PNG.png"/></a>
-               <a  href="tel:123-456-7890"><img className="imgfooter"src="https://lh3.googleusercontent.com/proxy/N39GopOkAPT_bbQxYTDZrsuzm9q4QtmVE3R3X4uoWzXNHcE_bbZUqORyWRafv-SWcs8qNLrRn-EzGJOCcKB0OqLksVwnlUtxieuBobGYAeCxHteYX_J1nRH_jDEIO_n9CEoxPXcpU4QD"/></a>
+               <a  href="tel:301-339-3636"><img className="imgfooter"src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcReFldAe2KbdzYP-InVc5OzA22dW2Bi2J6cffUYdBNAy92SPWSB"/></a>
 
     </div>
     )
--- project/personalportfolio/src/Project.js
+++ project/personalportfolio/src/Project.js
@@ -1,9 +1,10 @@
 import React from "react"
+import currencyPreview from './currency-preview.png'
 
 function Project(){
     return(
-    <div>
-        project:
+    <div className="progect-holder">
+        <a href="http://currencyexchange.surge.sh/"><img className="progect" src={currencyPreview}/></a>
     </div>
     )
 }
