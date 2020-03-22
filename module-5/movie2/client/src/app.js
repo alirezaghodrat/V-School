84e65193869fe0b3227a616104c7af7ed8661d16
ali ghodratipour <alireza_ghodratipour@yahoo.com>
2020-03-22 18:42:29 -0400

    here is some change

--- module-5/movie2/client/src/app.js
+++ module-5/movie2/client/src/app.js
@@ -19,6 +19,7 @@ export default function App (){
            .then(res => console.log(res))
            .catch(err => console.log(err))
        )
+       
    }
     useEffect(()=>{
         getMovie()
