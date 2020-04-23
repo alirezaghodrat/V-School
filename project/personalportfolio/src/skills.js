3204daab31d2e90e8afb59cfa95b685a22c9881f
ali ghodratipour <alireza_ghodratipour@yahoo.com>
2020-04-23 16:18:18 -0400

    personal portfolio

--- project/personalportfolio/src/skills.js
+++ project/personalportfolio/src/skills.js
@@ -23,8 +23,6 @@ function Contact(){
                             <li>CSS3/SASS</li>
                         </ul>
                     </div>
-                    <>
-                    </>
                     <div className="each-skill-div">
                          <h3>Frameworks</h3>
                          <ul>
