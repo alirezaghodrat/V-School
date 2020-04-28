b6e87dfc6f7dd3444cd85b24df86bf23ad208cb4
ali ghodratipour <alireza_ghodratipour@yahoo.com>
2020-04-28 10:31:17 -0400

    porfolio

--- project/personalportfolio/src/Footer.js
+++ project/personalportfolio/src/Footer.js
@@ -7,7 +7,7 @@ function Footer(){
                     <img className="imgfooter" src="http://pngimg.com/uploads/github/github_PNG40.png" /></a>
                 <a href="https://www.linkedin.com/in/alireza-godratipour/">
                     <img className="imgfooter" src="https://1000logos.net/wp-content/uploads/2017/03/LinkedIn-Logo.png" /></a>
-                    <a href="file:///Users/alirezaghodratipour/Downloads/Copy%20of%20Copy%20of%20Resume%20draft%20no.2%20-%201%20_%202020%20(1).pdf" download>
+                    <a href="Ali's resume (2).pdf" download>
                     <img className="imgfooter" src="http://www.pngall.com/wp-content/uploads/2016/04/Resume-PNG.png"/></a>
        </footer>
     )
