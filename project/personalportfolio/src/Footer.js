cc97a2821c9a52c2f841a21499dc2643e7dbf3b0
ali ghodratipour <alireza_ghodratipour@yahoo.com>
2020-05-01 21:06:57 -0400

    portfolio

--- project/personalportfolio/src/Footer.js
+++ project/personalportfolio/src/Footer.js
@@ -1,5 +1,5 @@
 import React from "react"
-// import resume from "resume.png"
+import resume from "./mypdf/resume22.pdf"
 
 function Footer(){
     return(
@@ -8,7 +8,8 @@ function Footer(){
                     <img className="imgfooter" src="http://pngimg.com/uploads/github/github_PNG40.png" /></a>
                 <a href="https://www.linkedin.com/in/alireza-godratipour/">
                     <img className="imgfooter" src="https://1000logos.net/wp-content/uploads/2017/03/LinkedIn-Logo.png" /></a>
-                    <a src="">
+                <a href={resume} download>
+                {/* <a href={resume} target="blank" > */}
                     <img className="imgfooter" src="http://www.pngall.com/wp-content/uploads/2016/04/Resume-PNG.png"/></a>
        </footer>
     )
