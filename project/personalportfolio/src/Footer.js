15ebcee379d404e150ec12b700d45fab03b049a1
ali ghodratipour <alireza_ghodratipour@yahoo.com>
2020-05-02 15:44:24 -0400

    portfolio

--- project/personalportfolio/src/Footer.js
+++ project/personalportfolio/src/Footer.js
@@ -8,8 +8,8 @@ function Footer(){
                     <img className="imgfooter" src="http://pngimg.com/uploads/github/github_PNG40.png" /></a>
                 <a href="https://www.linkedin.com/in/alireza-godratipour/">
                     <img className="imgfooter" src="https://1000logos.net/wp-content/uploads/2017/03/LinkedIn-Logo.png" /></a>
-                <a href={resume} download>
-                {/* <a href={resume} target="blank" > */}
+                {/* <a href={resume} download> */}
+                <a href={resume} target="blank" >
                     <img className="imgfooter" src="http://www.pngall.com/wp-content/uploads/2016/04/Resume-PNG.png"/></a>
        </footer>
     )
